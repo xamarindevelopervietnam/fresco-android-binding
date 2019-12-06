fcfb60117d876792b181e81a01f7beeaf4849cfe
kenyd <chitrung09t2@gmail.com>
2019-12-06 13:51:03 +0700

    upgrade target framework

--- Fresco.Test/Resources/Resource.Designer.cs
+++ Fresco.Test/Resources/Resource.Designer.cs
@@ -31,6 +31,99 @@ namespace Fresco.Test
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7F010000
+			public const int actualImageResource = 2130771968;
+			
+			// aapt resource value: 0x7F010001
+			public const int actualImageScaleType = 2130771969;
+			
+			// aapt resource value: 0x7F010002
+			public const int actualImageUri = 2130771970;
+			
+			// aapt resource value: 0x7F010003
+			public const int backgroundImage = 2130771971;
+			
+			// aapt resource value: 0x7F010004
+			public const int fadeDuration = 2130771972;
+			
+			// aapt resource value: 0x7F010005
+			public const int failureImage = 2130771973;
+			
+			// aapt resource value: 0x7F010006
+			public const int failureImageScaleType = 2130771974;
+			
+			// aapt resource value: 0x7F010007
+			public const int overlayImage = 2130771975;
+			
+			// aapt resource value: 0x7F010008
+			public const int placeholderImage = 2130771976;
+			
+			// aapt resource value: 0x7F010009
+			public const int placeholderImageScaleType = 2130771977;
+			
+			// aapt resource value: 0x7F01000A
+			public const int pressedStateOverlayImage = 2130771978;
+			
+			// aapt resource value: 0x7F01000B
+			public const int progressBarAutoRotateInterval = 2130771979;
+			
+			// aapt resource value: 0x7F01000C
+			public const int progressBarImage = 2130771980;
+			
+			// aapt resource value: 0x7F01000D
+			public const int progressBarImageScaleType = 2130771981;
+			
+			// aapt resource value: 0x7F01000E
+			public const int retryImage = 2130771982;
+			
+			// aapt resource value: 0x7F01000F
+			public const int retryImageScaleType = 2130771983;
+			
+			// aapt resource value: 0x7F010010
+			public const int roundAsCircle = 2130771984;
+			
+			// aapt resource value: 0x7F010011
+			public const int roundBottomEnd = 2130771985;
+			
+			// aapt resource value: 0x7F010012
+			public const int roundBottomLeft = 2130771986;
+			
+			// aapt resource value: 0x7F010013
+			public const int roundBottomRight = 2130771987;
+			
+			// aapt resource value: 0x7F010014
+			public const int roundBottomStart = 2130771988;
+			
+			// aapt resource value: 0x7F01001A
+			public const int roundedCornerRadius = 2130771994;
+			
+			// aapt resource value: 0x7F01001B
+			public const int roundingBorderColor = 2130771995;
+			
+			// aapt resource value: 0x7F01001C
+			public const int roundingBorderPadding = 2130771996;
+			
+			// aapt resource value: 0x7F01001D
+			public const int roundingBorderWidth = 2130771997;
+			
+			// aapt resource value: 0x7F010015
+			public const int roundTopEnd = 2130771989;
+			
+			// aapt resource value: 0x7F010016
+			public const int roundTopLeft = 2130771990;
+			
+			// aapt resource value: 0x7F010017
+			public const int roundTopRight = 2130771991;
+			
+			// aapt resource value: 0x7F010018
+			public const int roundTopStart = 2130771992;
+			
+			// aapt resource value: 0x7F010019
+			public const int roundWithOverlayColor = 2130771993;
+			
+			// aapt resource value: 0x7F01001E
+			public const int viewAspectRatio = 2130771998;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -44,20 +137,50 @@ namespace Fresco.Test
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int basic_image = 2130968576;
+			// aapt resource value: 0x7F020000
+			public const int basic_image = 2130837504;
 			
-			// aapt resource value: 0x7f040004
-			public const int gif_animated = 2130968580;
+			// aapt resource value: 0x7F020001
+			public const int center = 2130837505;
 			
-			// aapt resource value: 0x7f040003
-			public const int webp_animated = 2130968579;
+			// aapt resource value: 0x7F020002
+			public const int centerCrop = 2130837506;
 			
-			// aapt resource value: 0x7f040001
-			public const int webp_static = 2130968577;
+			// aapt resource value: 0x7F020003
+			public const int centerInside = 2130837507;
 			
-			// aapt resource value: 0x7f040002
-			public const int webp_translucent = 2130968578;
+			// aapt resource value: 0x7F020004
+			public const int fitBottomStart = 2130837508;
+			
+			// aapt resource value: 0x7F020005
+			public const int fitCenter = 2130837509;
+			
+			// aapt resource value: 0x7F020006
+			public const int fitEnd = 2130837510;
+			
+			// aapt resource value: 0x7F020007
+			public const int fitStart = 2130837511;
+			
+			// aapt resource value: 0x7F020008
+			public const int fitXY = 2130837512;
+			
+			// aapt resource value: 0x7F020009
+			public const int focusCrop = 2130837513;
+			
+			// aapt resource value: 0x7F02000A
+			public const int gif_animated = 2130837514;
+			
+			// aapt resource value: 0x7F02000B
+			public const int none = 2130837515;
+			
+			// aapt resource value: 0x7F02000C
+			public const int webp_animated = 2130837516;
+			
+			// aapt resource value: 0x7F02000D
+			public const int webp_static = 2130837517;
+			
+			// aapt resource value: 0x7F02000E
+			public const int webp_translucent = 2130837518;
 			
 			static Id()
 			{
@@ -72,8 +195,8 @@ namespace Fresco.Test
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			// aapt resource value: 0x7F030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -88,8 +211,8 @@ namespace Fresco.Test
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7F040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
@@ -97,6 +220,265 @@ namespace Fresco.Test
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			// aapt resource value: { 0x7F010001,0x7F010003,0x7F010004,0x7F010005,0x7F010006,0x7F010007,0x7F010008,0x7F010009,0x7F01000A,0x7F01000B,0x7F01000C,0x7F01000D,0x7F01000E,0x7F01000F,0x7F010010,0x7F010011,0x7F010012,0x7F010013,0x7F010014,0x7F010015,0x7F010016,0x7F010017,0x7F010018,0x7F010019,0x7F01001A,0x7F01001B,0x7F01001C,0x7F01001D,0x7F01001E }
+			public static int[] GenericDraweeHierarchy = new int[] {
+					2130771969,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978,
+					2130771979,
+					2130771980,
+					2130771981,
+					2130771982,
+					2130771983,
+					2130771984,
+					2130771985,
+					2130771986,
+					2130771987,
+					2130771988,
+					2130771989,
+					2130771990,
+					2130771991,
+					2130771992,
+					2130771993,
+					2130771994,
+					2130771995,
+					2130771996,
+					2130771997,
+					2130771998};
+			
+			// aapt resource value: 0
+			public const int GenericDraweeHierarchy_actualImageScaleType = 0;
+			
+			// aapt resource value: 1
+			public const int GenericDraweeHierarchy_backgroundImage = 1;
+			
+			// aapt resource value: 2
+			public const int GenericDraweeHierarchy_fadeDuration = 2;
+			
+			// aapt resource value: 3
+			public const int GenericDraweeHierarchy_failureImage = 3;
+			
+			// aapt resource value: 4
+			public const int GenericDraweeHierarchy_failureImageScaleType = 4;
+			
+			// aapt resource value: 5
+			public const int GenericDraweeHierarchy_overlayImage = 5;
+			
+			// aapt resource value: 6
+			public const int GenericDraweeHierarchy_placeholderImage = 6;
+			
+			// aapt resource value: 7
+			public const int GenericDraweeHierarchy_placeholderImageScaleType = 7;
+			
+			// aapt resource value: 8
+			public const int GenericDraweeHierarchy_pressedStateOverlayImage = 8;
+			
+			// aapt resource value: 9
+			public const int GenericDraweeHierarchy_progressBarAutoRotateInterval = 9;
+			
+			// aapt resource value: 10
+			public const int GenericDraweeHierarchy_progressBarImage = 10;
+			
+			// aapt resource value: 11
+			public const int GenericDraweeHierarchy_progressBarImageScaleType = 11;
+			
+			// aapt resource value: 12
+			public const int GenericDraweeHierarchy_retryImage = 12;
+			
+			// aapt resource value: 13
+			public const int GenericDraweeHierarchy_retryImageScaleType = 13;
+			
+			// aapt resource value: 14
+			public const int GenericDraweeHierarchy_roundAsCircle = 14;
+			
+			// aapt resource value: 15
+			public const int GenericDraweeHierarchy_roundBottomEnd = 15;
+			
+			// aapt resource value: 16
+			public const int GenericDraweeHierarchy_roundBottomLeft = 16;
+			
+			// aapt resource value: 17
+			public const int GenericDraweeHierarchy_roundBottomRight = 17;
+			
+			// aapt resource value: 18
+			public const int GenericDraweeHierarchy_roundBottomStart = 18;
+			
+			// aapt resource value: 24
+			public const int GenericDraweeHierarchy_roundedCornerRadius = 24;
+			
+			// aapt resource value: 25
+			public const int GenericDraweeHierarchy_roundingBorderColor = 25;
+			
+			// aapt resource value: 26
+			public const int GenericDraweeHierarchy_roundingBorderPadding = 26;
+			
+			// aapt resource value: 27
+			public const int GenericDraweeHierarchy_roundingBorderWidth = 27;
+			
+			// aapt resource value: 19
+			public const int GenericDraweeHierarchy_roundTopEnd = 19;
+			
+			// aapt resource value: 20
+			public const int GenericDraweeHierarchy_roundTopLeft = 20;
+			
+			// aapt resource value: 21
+			public const int GenericDraweeHierarchy_roundTopRight = 21;
+			
+			// aapt resource value: 22
+			public const int GenericDraweeHierarchy_roundTopStart = 22;
+			
+			// aapt resource value: 23
+			public const int GenericDraweeHierarchy_roundWithOverlayColor = 23;
+			
+			// aapt resource value: 28
+			public const int GenericDraweeHierarchy_viewAspectRatio = 28;
+			
+			// aapt resource value: { 0x7F010000,0x7F010001,0x7F010002,0x7F010003,0x7F010004,0x7F010005,0x7F010006,0x7F010007,0x7F010008,0x7F010009,0x7F01000A,0x7F01000B,0x7F01000C,0x7F01000D,0x7F01000E,0x7F01000F,0x7F010010,0x7F010011,0x7F010012,0x7F010013,0x7F010014,0x7F010015,0x7F010016,0x7F010017,0x7F010018,0x7F010019,0x7F01001A,0x7F01001B,0x7F01001C,0x7F01001D,0x7F01001E }
+			public static int[] SimpleDraweeView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978,
+					2130771979,
+					2130771980,
+					2130771981,
+					2130771982,
+					2130771983,
+					2130771984,
+					2130771985,
+					2130771986,
+					2130771987,
+					2130771988,
+					2130771989,
+					2130771990,
+					2130771991,
+					2130771992,
+					2130771993,
+					2130771994,
+					2130771995,
+					2130771996,
+					2130771997,
+					2130771998};
+			
+			// aapt resource value: 0
+			public const int SimpleDraweeView_actualImageResource = 0;
+			
+			// aapt resource value: 1
+			public const int SimpleDraweeView_actualImageScaleType = 1;
+			
+			// aapt resource value: 2
+			public const int SimpleDraweeView_actualImageUri = 2;
+			
+			// aapt resource value: 3
+			public const int SimpleDraweeView_backgroundImage = 3;
+			
+			// aapt resource value: 4
+			public const int SimpleDraweeView_fadeDuration = 4;
+			
+			// aapt resource value: 5
+			public const int SimpleDraweeView_failureImage = 5;
+			
+			// aapt resource value: 6
+			public const int SimpleDraweeView_failureImageScaleType = 6;
+			
+			// aapt resource value: 7
+			public const int SimpleDraweeView_overlayImage = 7;
+			
+			// aapt resource value: 8
+			public const int SimpleDraweeView_placeholderImage = 8;
+			
+			// aapt resource value: 9
+			public const int SimpleDraweeView_placeholderImageScaleType = 9;
+			
+			// aapt resource value: 10
+			public const int SimpleDraweeView_pressedStateOverlayImage = 10;
+			
+			// aapt resource value: 11
+			public const int SimpleDraweeView_progressBarAutoRotateInterval = 11;
+			
+			// aapt resource value: 12
+			public const int SimpleDraweeView_progressBarImage = 12;
+			
+			// aapt resource value: 13
+			public const int SimpleDraweeView_progressBarImageScaleType = 13;
+			
+			// aapt resource value: 14
+			public const int SimpleDraweeView_retryImage = 14;
+			
+			// aapt resource value: 15
+			public const int SimpleDraweeView_retryImageScaleType = 15;
+			
+			// aapt resource value: 16
+			public const int SimpleDraweeView_roundAsCircle = 16;
+			
+			// aapt resource value: 17
+			public const int SimpleDraweeView_roundBottomEnd = 17;
+			
+			// aapt resource value: 18
+			public const int SimpleDraweeView_roundBottomLeft = 18;
+			
+			// aapt resource value: 19
+			public const int SimpleDraweeView_roundBottomRight = 19;
+			
+			// aapt resource value: 20
+			public const int SimpleDraweeView_roundBottomStart = 20;
+			
+			// aapt resource value: 26
+			public const int SimpleDraweeView_roundedCornerRadius = 26;
+			
+			// aapt resource value: 27
+			public const int SimpleDraweeView_roundingBorderColor = 27;
+			
+			// aapt resource value: 28
+			public const int SimpleDraweeView_roundingBorderPadding = 28;
+			
+			// aapt resource value: 29
+			public const int SimpleDraweeView_roundingBorderWidth = 29;
+			
+			// aapt resource value: 21
+			public const int SimpleDraweeView_roundTopEnd = 21;
+			
+			// aapt resource value: 22
+			public const int SimpleDraweeView_roundTopLeft = 22;
+			
+			// aapt resource value: 23
+			public const int SimpleDraweeView_roundTopRight = 23;
+			
+			// aapt resource value: 24
+			public const int SimpleDraweeView_roundTopStart = 24;
+			
+			// aapt resource value: 25
+			public const int SimpleDraweeView_roundWithOverlayColor = 25;
+			
+			// aapt resource value: 30
+			public const int SimpleDraweeView_viewAspectRatio = 30;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
